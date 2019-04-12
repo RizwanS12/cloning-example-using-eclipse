e1283144ad3128dc1439955acda0c1735231059f
PoojaNakar <46945168+PoojaNakar@users.noreply.github.com>
2019-04-11 23:39:51 -0400

    commit

---
+++ sparkexamples01/src/main/java/com/lambton/spark/training01/sparkexamples01/App.java
@@ -0,0 +1,13 @@
+package com.lambton.spark.training01.sparkexamples01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
